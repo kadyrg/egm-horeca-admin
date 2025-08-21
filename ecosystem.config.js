52ed9854698c6ea4808fa95a687e1e4336d4ba10
Kadyr Gullyyev <kadyr.gullyyew@gmail.com>
2025-08-21 16:22:19 +0200

    c

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+const { resolve } = require("path");
+
+module.exports = {
+  apps: [
+    {
+      name: "admin-app",
+      script: "npm",
+      args: "start",
+      cwd: resolve(__dirname, "."),
+      env: {
+        NODE_ENV: "production",
+        PORT: "3001"
+      },
+      watch: false
+    }
+  ]
+};
